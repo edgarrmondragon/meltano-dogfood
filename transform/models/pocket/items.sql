669baabed7b83000fc5ad5e23f5a69b2b8a7a932
Edgar Ramírez <edgarrm358@gmail.com>
2022-05-07 19:47:47 -0500

    chore: add SQLFluff (#2)
    * chore: add SQLFluff
    
    * chore: add SQLFluff job

--- transform/models/pocket/items.sql
+++ transform/models/pocket/items.sql
@@ -3,6 +3,7 @@ select
   , given_url
   , given_title
   , datetime(time_added, 'unixepoch') as added_at
-  , case when time_read > 0 then datetime(time_read, 'unixepoch') else null end as read_at
+  , case when time_read > 0 then datetime(time_read, 'unixepoch') end as read_at
   , datetime(time_updated, 'unixepoch') as updated_at
+  , time_to_read
 from {{ source('pocket', 'items') }}
