c8a818f9cf00e074c4ea65be964833f866339140
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2022-05-09 20:06:49 -0500

    fix model

--- transform/models/pocket/items.sql
+++ transform/models/pocket/items.sql
@@ -2,8 +2,8 @@ select
   item_id
   , given_url
   , given_title
+  , time_to_read
   , datetime(time_added, 'unixepoch') as added_at
   , case when time_read > 0 then datetime(time_read, 'unixepoch') end as read_at
   , datetime(time_updated, 'unixepoch') as updated_at
-  , time_to_read
 from {{ source('pocket', 'items') }}
