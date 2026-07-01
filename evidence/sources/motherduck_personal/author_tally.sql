252c27a970330b049c3ffbdc71918a96afc6dd75
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2026-06-30 18:20:52 -0600

    Clean up Evidence files
    Signed-off-by: Edgar Ramírez Mondragón <edgarrm358@gmail.com>

--- evidence/sources/motherduck_personal/author_tally.sql
+++
@@ -1,7 +0,0 @@
-select
-  author_name
-  , count(*) as total
-from personal.authors
-group by author_name
-order by total desc
-limit 5
--- evidence/sources/motherduck_personal/pending_pocket_reads.sql
+++
@@ -1,8 +0,0 @@
-select
-  item_id
-  , added_at
-  , resolved_title
-  , time_to_read
-  , given_url
-from personal.items
-where read_at is null
